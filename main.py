4e28e50e810738989ba0158d560f193811ba00ef
JordanB001 <jordan.bigre@hotmail.com>
2024-11-17 14:57:38 +0100

    clean main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from sqlalchemy import *
 
 from connect_to_DB import connect_to_database
-from CRUD import read
+import CRUD
 from console import console
 
 load_dotenv()
@@ -19,5 +19,3 @@ if __name__ == "__main__":
     # Connect to Database
     engine = connect_to_database(type_of_DB="postgres" ,username=USERNAME_DB, password=PASSWORD_DB,
                                     host=HOST_DB, name_of_database=NAME_OF_DB)
-
-    read(engine, "utilisateur", name_of_column="*")
