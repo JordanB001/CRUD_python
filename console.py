d66f8ca9f32e021262493c72edb24686b0e601ce
JordanB001 <jordan.bigre@hotmail.com>
2024-11-17 21:11:27 +0100

    add command with no transaction in function console

--- console.py
+++ console.py
@@ -5,13 +5,34 @@ from Errors.exceptions import IncorectCommand
 
 
 def console(engine, command):
+    list_no_transaction = ["CREATE DATABASE", "DROP DATABASE", "BEGIN", "COMMIT", "ROLLBACK", "SET TRANSACTION",
+                           "SAVEPOINT", "RELEASE SAVEPOINT", "VACUUM", "CLUSTER", "REINDEX DATABASE", "REINDEX SYSTEM",
+                           "ANALYZE", "CREATE USER", "CREATE ROLE", "ALTER SYSTEM", ]
 
-    try:
-        with engine.connect() as engine_connected:
-            with engine_connected.begin():
+    if any(no_transaction in command.upper() for no_transaction in list_no_transaction):
+        #command with no transaction
+        try:
+            with engine.connect().execution_options(isolation_level="AUTOCOMMIT") as engine_connected:
+                print(f"Command SQL : {text(command)}")
                 result = engine_connected.execute(text(command))
-                return result
-    except ProgrammingError:
-        raise IncorectCommand
+
+                if result.returns_rows:
+                    return result.fetchall()
+                else:
+                    return "Command execute successfully"
+
+        except Exception as error:
+            print(error)
+
+    else:
+        #command with transaction
+        try:
+            with engine.connect() as engine_connected:
+                with engine_connected.begin():
+                    print(f"Command SQL : {text(command)}")
+                    result = engine_connected.execute(text(command))
+                    return result
+        except ProgrammingError:
+            raise IncorectCommand
 
 
